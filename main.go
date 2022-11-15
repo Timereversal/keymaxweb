343f885d32ccbf0e7c5b03ecef1baa95446c19dd
timereversal <berzoc@gmail.com>
2022-11-14 19:19:25 -0500

    feature1 in progreess

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	carsC := controllers.Car{}
+	r.Post("/cars/new", carsC.New)
+	err := http.ListenAndServe("8080", r)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ models/cars.go
@@ -0,0 +1,10 @@
+package models
+
+type Car struct {
+	brand string
+	model string
+}
+
+func (c *Car) create(brand, model string) {
+
+}
